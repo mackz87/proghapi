79143b2da6ac958149d5d66f1273a94fc85a7b75
patrick <pat@ric.k>
2017-05-08 19:00:27 +0100

    update

--- serviceworker.js
+++ serviceworker.js
@@ -4,7 +4,7 @@ var CACHED_URLS = [
     // Our HTML
     BASE_PATH + 'index.html',
 
-    // Images for favicons
+  
     BASE_PATH + 'img/android-icon-36x36.png',
     BASE_PATH + 'img/android-icon-48x48.png',
     BASE_PATH + 'img/android-icon-72x72.png',
@@ -13,45 +13,17 @@ var CACHED_URLS = [
     BASE_PATH + 'img/android-icon-192x192.png',
     BASE_PATH + 'img/favicon-32x32.png',
 
-    //Images for page
-    BASE_PATH + 'img/offlinemap.jpg',
-    BASE_PATH + 'img/dino.png',
-    BASE_PATH + 'img/jack.jpg',
-    BASE_PATH + 'img/paddy.jpg',
+
     BASE_PATH + 'img/favicon.ico',
-    BASE_PATH + 'img/favicon-16x16.png',
-    BASE_PATH + 'img/favicon-32x32.png',
-    BASE_PATH + 'img/favicon-96x96.png',
-    BASE_PATH + 'img/ms-icon-70x70.png',
-    BASE_PATH + 'img/ms-icon-144x144.png',
-    BASE_PATH + 'img/ms-icon-150x150.png',
-    BASE_PATH + 'img/ms-icon-310x310.png',
-    BASE_PATH + 'eventsimages/example-blog01.jpg',
-    BASE_PATH + 'eventsimages/example-blog02.jpg',
-    BASE_PATH + 'eventsimages/example-blog03.jpg',
-    BASE_PATH + 'eventsimages/example-blog04.jpg',
-    BASE_PATH + 'eventsimages/example-blog05.jpg',
-    BASE_PATH + 'eventsimages/example-blog06.jpg',
-    BASE_PATH + 'eventsimages/example-blog07.jpg',
-    BASE_PATH + 'eventsimages/example-work01.jpg',
-    BASE_PATH + 'eventsimages/example-work02.jpg',
-    BASE_PATH + 'eventsimages/example-work03.jpg',
-    BASE_PATH + 'eventsimages/example-work04.jpg',
-    BASE_PATH + 'eventsimages/example-work05.jpg',
-    BASE_PATH + 'eventsimages/example-work06.jpg',
-    BASE_PATH + 'eventsimages/example-work07.jpg',
-    BASE_PATH + 'eventsimages/example-work08.jpg',
-    BASE_PATH + 'eventsimages/example-work09.jpg',
-    // JavaScript
-    BASE_PATH + 'offline-map.js',
-    BASE_PATH + 'material.js',
+
+
+
+
     // Manifest
     BASE_PATH + 'manifest.json',
   // CSS and fonts
-    'https://fonts.googleapis.com/css?family=Roboto:regular,bold,italic,thin,light,bolditalic,black,medium&lang=en',
-    'https://fonts.googleapis.com/icon?family=Material+Icons',
-    BASE_PATH + 'min-style.css',
-    BASE_PATH + 'styles.css'
+
+    BASE_PATH + 'style.css'
 ];
 
 var googleMapsAPIJS = 'https://maps.googleapis.com/maps/api/js?key=YOURKEY&callback=initMap';
